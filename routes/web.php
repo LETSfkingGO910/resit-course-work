ec2bedc5f87998eac9239f1e24d7853cb62de468
LETSfkingGO910 <2731210660@qq.com>
2026-05-14 17:27:20 +0800

    Update routes and registration flow

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check()
+        ? redirect()->route('dashboard')
+        : redirect('/register');
 });
 
 Route::middleware(['auth'])->group(function () {
